9cd23dea9920a376c9bfd86a33607bc3146059d6
unknown <andregbisi@gmail.com>
2021-07-17 16:02:30 -0300

    primeiro commit

---
+++ agendaDeTarefasConsole/agendaDeTarefasConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace agendaDeTarefasConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
